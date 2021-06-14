d1ad914384f8a7222ec9384a57b44fad109b307d
Trung Nguyen <trungnv184@gmail.com>
2021-06-14 13:42:11 +0700

    chore: setup test and unit test for order controller

--- services/orders/src/orders/orders.controller.spec.ts
+++ services/orders/src/orders/orders.controller.spec.ts
@@ -1,9 +1,14 @@
+import { CreateOrderDto } from '@dto';
+import { OrderState } from '@common/enums/order-state.enum';
+import { PaymentsModule } from '@payments/payments.module';
+import { OrderConstant } from '@common/constants/order.constant';
 import { OrderQueueProcessor } from './order-queue.processor';
 import { Order } from './schemas/order.schema';
-import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { Test } from '@nestjs/testing';
 import { getModelToken } from '@nestjs/mongoose';
+import { BullModule } from '@nestjs/bull';
+import { OrdersService } from './orders.service';
 
 const ORDERS_MOCK = [
   {
@@ -22,17 +27,25 @@ const ORDERS_MOCK = [
       payMethod: 'Cash'
     },
     notes: 'Add book care service',
-    state: 'Created'
+    state: 'Created',
+    createdDate: '2021-06-14T04:33:01.086Z',
+    updatedDate: '2021-06-14T04:33:01.086Z'
   }
-] as Order[];
+] as unknown as Order[];
 
-describe('CatsController', () => {
+describe('OrdersController', () => {
   let ordersController: OrdersController;
   let orderService: OrdersService;
   let orderQueueProcessor: OrderQueueProcessor;
 
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
+      imports: [
+        BullModule.registerQueue({
+          name: OrderConstant.ORDER_QUEUE_NAME
+        }),
+        PaymentsModule
+      ],
       controllers: [OrdersController],
       providers: [
         OrdersService,
@@ -49,10 +62,61 @@ describe('CatsController', () => {
     orderQueueProcessor = moduleRef.get<OrderQueueProcessor>(OrderQueueProcessor);
   });
 
-  describe('getAllOrders', () => {
-    it('should return an array of cats', async () => {
-      jest.spyOn(orderService, 'all').mockResolvedValue(ORDERS_MOCK);
-      expect(await ordersController.getAllOrders()).toBe(ORDERS_MOCK);
+  it('should create instance orders controller successfully', async () => {
+    expect(ordersController).toBeDefined();
+  });
+  it('should return list of orders successfully', async () => {
+    jest.spyOn(orderService, 'all').mockResolvedValue(ORDERS_MOCK);
+    expect(await ordersController.getAllOrders()).toBe(ORDERS_MOCK);
+  });
+
+  it('should Fetch order by id successfully', async () => {
+    const orderMock = {
+      ...ORDERS_MOCK[0]
+    } as Order;
+    const orderId = '60c6dbfd040f89585a7a8f11';
+
+    jest.spyOn(orderService, 'findOne').mockResolvedValue(orderMock);
+    expect(await ordersController.fetchOrderById(orderId)).toBe(orderMock);
+  });
+
+  describe('getOderStatus', () => {
+    it('should get order status by id successfully', async () => {
+      const orderMock = {
+        ...ORDERS_MOCK[0],
+        state: OrderState.CONFIRMED
+      } as unknown as Order;
+      const orderId = '60c6dbfd040f89585a7a8f11';
+
+      jest.spyOn(orderService, 'findOne').mockResolvedValue(orderMock);
+      expect(await ordersController.getOderStatus(orderId)).toBe(OrderState.CONFIRMED);
     });
+
+    it('should get order status by id failed', async () => {
+      const orderMock = {
+        ...ORDERS_MOCK[0],
+        state: OrderState.CONFIRMED
+      } as unknown as Order;
+
+      jest.spyOn(orderService, 'findOne').mockRejectedValue(new Error('OrderId not found'));
+      try {
+        const orderId = '60c6dbfd040f89585a7a8f11';
+        await ordersController.getOderStatus(orderId);
+      } catch (err) {
+        expect(err.message).toContain('OrderId not found');
+      }
+    });
+  });
+
+  it('should get order status by id successfully', async () => {
+    const orderMock = {
+      ...ORDERS_MOCK[0]
+    } as unknown as Order;
+
+    const createdOrderDto = {
+      ...orderMock
+    } as CreateOrderDto;
+    jest.spyOn(orderService, 'create').mockResolvedValue(orderMock);
+    expect(await ordersController.createOrder(createdOrderDto)).toBe(orderMock);
   });
 });
