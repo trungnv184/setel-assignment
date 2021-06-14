92d98d075e87dd112279498e30a23e5748de5807
Trung Nguyen <trungnv184@gmail.com>
2021-06-14 17:11:04 +0700

    feat: update unit test and e2e test

---
+++ services/orders/src/orders/orders.service.spec.ts
@@ -0,0 +1,154 @@
+import { UpdatedOrderDto } from './dto/updated-order.dto';
+import { OrderState } from './common/enums/order-state.enum';
+import { OrderModel, OrderSchema } from './schemas/order.schema';
+import { CreateOrderDto } from './dto/created-order.dto';
+import { PaymentsModule } from '@payments/payments.module';
+import { OrderConstant } from '@common/constants/order.constant';
+import { OrderQueueProcessor } from './order-queue.processor';
+import { Order } from '@schemas';
+import { Test } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/mongoose';
+import { BullModule } from '@nestjs/bull';
+import { OrdersService } from './orders.service';
+import { Model } from 'mongoose';
+
+const ORDERS_MOCK = [
+  {
+    orderItems: [
+      {
+        productName: 'The Clean Architecture',
+        productUrl: 'http://amazon.com/books/it/clean-architecture',
+        price: 50000,
+        quantity: 2
+      }
+    ],
+    metadata: {
+      customerName: 'Trung Nguyen',
+      address: 'Ho Chi Minh City',
+      phoneNumber: '0906925896',
+      payMethod: 'Cash'
+    },
+    notes: 'Add book care service',
+    state: 'Created',
+    createdDate: '2021-06-14T04:33:01.086Z',
+    updatedDate: '2021-06-14T04:33:01.086Z'
+  }
+] as unknown as Order[];
+
+const getCreatedOrderDto = (customData: any = {}) => {
+  return {
+    ...ORDERS_MOCK[0],
+    ...customData
+  } as CreateOrderDto;
+};
+
+describe('OrdersController', () => {
+  let orderService: OrdersService;
+  let orderQueueProcessor: OrderQueueProcessor;
+  let orderModel: Model<OrderModel>;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [
+        BullModule.registerQueue({
+          name: OrderConstant.ORDER_QUEUE_NAME
+        }),
+        PaymentsModule
+      ],
+      providers: [
+        OrdersService,
+        OrderQueueProcessor,
+        {
+          provide: getModelToken(Order.name),
+          useValue: {
+            create: jest.fn().mockImplementation((dto) => {
+              return {
+                ...dto,
+                _id: '1212'
+              };
+            }),
+            find: jest.fn().mockImplementation(() => {
+              return ORDERS_MOCK;
+            }),
+            findById: jest.fn(),
+            findOneAndUpdate: jest.fn()
+          }
+        }
+      ]
+    }).compile();
+
+    orderService = moduleRef.get<OrdersService>(OrdersService);
+    orderQueueProcessor = moduleRef.get<OrderQueueProcessor>(OrderQueueProcessor);
+    orderModel = moduleRef.get<Model<OrderModel>>(getModelToken(Order.name));
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('should create instance order service', () => {
+    expect(orderService).toBeDefined();
+  });
+
+  it('should create order service successfully', async () => {
+    const createdOrderDto = getCreatedOrderDto();
+    jest.spyOn(orderQueueProcessor, 'addOrderForPaymentProcessing').mockImplementation(() => {
+      return Promise.resolve();
+    });
+    const response = await orderService.create(createdOrderDto);
+    expect((response as any)._id).toBe('1212');
+  });
+
+  it('should find all orders successfully', async () => {
+    jest.spyOn(orderModel, 'find').mockImplementation(() => {
+      return {
+        exec: jest.fn().mockResolvedValue(ORDERS_MOCK)
+      } as any;
+    });
+
+    const response = await orderService.all();
+    expect(response.length).toBe(1);
+  });
+
+  it('should find one order successfully', async () => {
+    jest.spyOn(orderModel, 'findById').mockResolvedValue({
+      ...ORDERS_MOCK[0],
+      state: OrderState.DELIVERED
+    } as unknown as OrderModel);
+
+    const response = await orderService.findOne('60c6dbfd040f89585a7a8f11');
+    expect(response.orderItems.length).toBe(1);
+    expect(response.state).toBe(OrderState.DELIVERED);
+  });
+
+  it('should update order successfully', async () => {
+    jest.spyOn(orderModel, 'findOneAndUpdate').mockImplementation((orderId, updateOrderDto) => {
+      return {
+        ...ORDERS_MOCK[0],
+        ...updateOrderDto,
+        _id: orderId
+      } as any;
+    });
+
+    const updateOrderDto = {
+      state: OrderState.CANCELLED
+    } as UpdatedOrderDto;
+
+    const response = await orderService.updateOrder('60c6dbfd040f89585a7a8f11', updateOrderDto);
+    expect(response.state).toBe(OrderState.CANCELLED);
+  });
+
+  it('should cancel order by id successfully', async () => {
+    jest.spyOn(orderModel, 'findById').mockResolvedValue(ORDERS_MOCK[0] as any);
+
+    jest.spyOn(orderModel, 'findOneAndUpdate').mockImplementation((orderId) => {
+      return {
+        ...ORDERS_MOCK[0],
+        state: OrderState.CANCELLED
+      } as any;
+    });
+
+    const response = await orderService.cancelOrderById('60c6dbfd040f89585a7a8f11');
+    expect(response.state).toBe(OrderState.CANCELLED);
+  });
+});
--- services/orders/test/app.e2e-spec.ts
+++ services/orders/test/app.e2e-spec.ts
@@ -1,21 +1,102 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import { CreateOrderDto } from './../src/orders/dto/created-order.dto';
 import { AppModule } from './../src/app.module';
+import * as request from 'supertest';
+import { Test } from '@nestjs/testing';
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import { OrdersService } from '@orders/orders.service';
+import { Order } from '@schemas';
 
-describe('AppController (e2e)', () => {
+const ORDERS_MOCK = [
+  {
+    orderItems: [
+      {
+        productName: 'The Clean Architecture',
+        productUrl: 'http://amazon.com/books/it/clean-architecture',
+        price: 50000,
+        quantity: 2
+      }
+    ],
+    metadata: {
+      customerName: 'Trung Nguyen',
+      address: 'Ho Chi Minh City',
+      phoneNumber: '0906925896',
+      payMethod: 'Cash'
+    },
+    notes: 'Add book care service',
+    state: 'Delivered',
+    createdDate: '2021-06-14T04:33:01.086Z',
+    updatedDate: '2021-06-14T04:33:01.086Z'
+  }
+] as unknown as Order[];
+
+describe('Orders', () => {
   let app: INestApplication;
+  const orderService = {
+    all: () => {
+      return {
+        data: ORDERS_MOCK
+      };
+    },
+    findOne: () => {
+      return ORDERS_MOCK[0];
+    },
+    create: () => {
+      return {
+        ...ORDERS_MOCK[0],
+        state: 'Created'
+      };
+    }
+  };
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule]
-    }).compile();
+    })
+      .overrideProvider(OrdersService)
+      .useValue(orderService)
+      .compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
     await app.init();
   });
 
-  it.skip('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
+  it(`GET /v1/api/orders`, () => {
+    return request(app.getHttpServer()).get('/orders').expect(HttpStatus.OK).expect(orderService.all());
+  });
+
+  it(`GET /v1/api/orders/:id/status`, () => {
+    const orderId = '60c720112badcc845396a807';
+    return request(app.getHttpServer()).get(`/orders/${orderId}/status`).expect(HttpStatus.OK);
+  });
+
+  it(`POST /v1/api/orders`, () => {
+    const payload = {
+      orderItems: [
+        {
+          productName: 'React Advantages',
+          productUrl: 'amazon.com/react-advantage',
+          price: 50000,
+          quantity: 2
+        },
+        {
+          productName: 'NestJs For Beginner',
+          productUrl: 'amazon.com/nest-js-beginner',
+          price: 50000,
+          quantity: 2
+        }
+      ],
+      metadata: {
+        customerName: 'Trung Nguyen',
+        address: 'Ho Chi Minh',
+        phoneNumber: '098562525',
+        payMethod: 'Cash'
+      }
+    } as unknown as CreateOrderDto;
+
+    return request(app.getHttpServer()).post(`/orders`).send(payload).expect(HttpStatus.CREATED);
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
