23de60f280384e710a840d21d76d6a667b73e90a
Trung Nguyen <trungnv184@gmail.com>
2021-06-14 21:47:40 +0700

    chore: setup payment service such as hybrid app

--- services/payments/src/main.ts
+++ services/payments/src/main.ts
@@ -1,4 +1,6 @@
+import { RABBIT_MQ_URL } from '@configs/environment.config';
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -7,6 +9,19 @@ async function bootstrap() {
   app.enableCors({
     origin: 'http://localhost:3000'
   });
+
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: [RABBIT_MQ_URL],
+      queue: 'payment_queue',
+      queueOptions: {
+        durable: false
+      }
+    }
+  });
+
+  await app.startAllMicroservicesAsync();
   await app.listen(8001);
 }
 bootstrap();
