6068f1acce458cf3cbf915ec4859607f9337fc3e
Trung Nguyen <trungnv184@gmail.com>
2021-06-20 12:01:24 +0700

    chore: custom layout header bar

--- clients/src/components/MenuBar/MenuBar.tsx
+++ clients/src/components/MenuBar/MenuBar.tsx
@@ -3,27 +3,25 @@ import { Icon, Menu } from 'semantic-ui-react';
 
 const MenuBar = () => {
   return (
-    <>
-      <Menu inverted={true} color="blue">
-        <Menu.Item name="home">
-          <Icon name="home" />
-          <Link to="/">
-            <strong>BOOK STORE</strong>
-          </Link>
-        </Menu.Item>
+    <Menu size="huge" inverted color="blue">
+      <Menu.Item name="home">
+        <Icon name="home" />
+        <Link to="/">
+          <strong>BOOK STORE</strong>
+        </Link>
+      </Menu.Item>
 
-        <Menu.Menu position="right">
-          <Menu.Item name="orders">
-            <Icon name="ordered list" />
-            <Link to="/orders">Your Orders</Link>
-          </Menu.Item>
-          <Menu.Item name="cart" active={true}>
-            <Icon name="cart" />
-            <Link to="/your-cart">Your Cart</Link>
-          </Menu.Item>
-        </Menu.Menu>
-      </Menu>
-    </>
+      <Menu.Menu position="right">
+        <Menu.Item name="orders">
+          <Icon name="ordered list" />
+          <Link to="/orders">Your Orders</Link>
+        </Menu.Item>
+        <Menu.Item name="cart" active={true}>
+          <Icon name="cart" />
+          <Link to="/your-cart">Your Cart</Link>
+        </Menu.Item>
+      </Menu.Menu>
+    </Menu>
   );
 };
 
