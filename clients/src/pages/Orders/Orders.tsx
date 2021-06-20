277e9783ad73538be43ecb9a35c38274c767609d
Trung Nguyen <trungnv184@gmail.com>
2021-06-20 15:02:32 +0700

    feat: display message to inform user when order page return empty data

--- clients/src/pages/Orders/Orders.tsx
+++ clients/src/pages/Orders/Orders.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { ErrorMessage, OrderItems } from 'components';
 import { useQuery } from '@apollo/client';
 import { GET_ORDER_LIST } from 'graphql/query';
-import { Dimmer, Loader } from 'semantic-ui-react';
+import { Dimmer, Loader, Message } from 'semantic-ui-react';
 import { OrderResponse } from 'types/order-response';
 import { StyledContainer } from 'components/Common/Style';
 import { useHistory } from 'react-router-dom';
 
 const Orders: React.FC<any> = () => {
-  const { data, loading, error } = useQuery(GET_ORDER_LIST);
+  const { data, loading, error } = useQuery(GET_ORDER_LIST, {
+    fetchPolicy: 'cache-and-network',
+  });
   const history = useHistory();
 
   if (loading) {
@@ -23,16 +25,27 @@ const Orders: React.FC<any> = () => {
 
   const orders = data?.getOrderList as OrderResponse[];
 
+  const renderOrderList = () => {
+    return orders.map((order) => (
+      <StyledContainer key={order.id} onClick={() => navigateToOrderDetailPage(order.id)}>
+        <OrderItems orderItems={order.orderItems} orderState={order.state} createdDate={order.createdDate} />
+      </StyledContainer>
+    ));
+  };
+
   const navigateToOrderDetailPage = (orderId: string) => {
     history.push(`/order-detail/${orderId}`);
   };
   return (
     <>
-      {orders.map((order) => (
-        <StyledContainer key={order.id} onClick={() => navigateToOrderDetailPage(order.id)}>
-          <OrderItems orderItems={order.orderItems} orderState={order.state} createdDate={order.createdDate} />
-        </StyledContainer>
-      ))}
+      {orders.length > 0 ? (
+        renderOrderList()
+      ) : (
+        <Message
+          header="Your order history"
+          content="You don't have any orders. Do you want to purchase some books today ?"
+        />
+      )}
     </>
   );
 };
